2942341d7e1fc71e60f48685c2edc43b43503687
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 19:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // justInvoke(fn): The function simply invokes the function passed
 // through to it. It also returns the return value of the passed
 // through function.
-
+function justInvoke(fn){
+  
+}
 // setThisWithCall(fn, thisValue, arg): The function again invokes
 // the function passed to it, but uses the call method to return the
 // function's this value. (Make sure to correctly pass the third
