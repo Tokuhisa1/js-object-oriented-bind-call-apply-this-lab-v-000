9de5eeded54a780d4d0704eb84764b9d73aa566c
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 19:38:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ function justInvoke(fn){
 // function's this value. (Make sure to correctly pass the third
 // argument!)
 function setThisWithCall(fn, thisValue, arg) {
-  
+
 }
 // setThisWithApply(fn, thisValue, args): Again, invoke the
 // function passed to it, change the this value of that function
 // passed to it. In addition, we ask you to invoked the passed
 // function with arguments. You should accomplish all of the above
 // by using apply.
-
+function setThisWithApply(fn, thisValue, args) {
+  
+}
 // returnNewFunctionOf(functionToBeCopied, thisValue): Here, we ask
 // you to write a function that returns a copy of the function
 // passed through, but sets the this value of the function's copy.
